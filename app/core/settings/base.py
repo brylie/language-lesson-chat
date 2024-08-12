bef6fea559776a540dbb50001233a64493c5c445
Brylie Christopher Oxley <brylie@protonmail.com>
2024-08-12 16:58:26 +0000

    Load dotenv

--- app/core/settings/base.py
+++ app/core/settings/base.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dotenv
+
+dotenv.load_dotenv()
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
