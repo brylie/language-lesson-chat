950f96c550a667db90083eaaadc2dee6584e3f38
Brylie Christopher Oxley <brylie@protonmail.com>
2024-08-20 00:45:03 +0300

    Add docstring

--- app/lessons/choices.py
+++ app/lessons/choices.py
@@ -3,6 +3,12 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CEFRLevel(models.TextChoices):
+    """
+    The Common European Framework of Reference for Languages (CEFR) is an international standard for describing language ability.
+
+    The CEFR levels are a way of describing a person's language proficiency in a foreign language. They are used by language schools and language testing organizations to evaluate a person's language proficiency.
+    """
+
     A1 = "a1", _("A1 - Beginner")
     A2 = "a2", _("A2 - Elementary")
     B1 = "b1", _("B1 - Intermediate")
