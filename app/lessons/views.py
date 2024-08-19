855eddfb97560f94c8fc8b48fc348f344cc34909
Brylie Christopher Oxley <brylie@protonmail.com>
2024-08-20 01:59:26 +0300

    Lint/debug

--- app/lessons/views.py
+++ app/lessons/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 import openai
 import os
-from pathlib import Path
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 
@@ -14,6 +12,7 @@ from .models import TranscriptMessage
 
 TTS_MODEL = "tts-1"
 
+
 class OpenAITTSVoices:
     ALLOY = "alloy"
     ECHO = "echo"
@@ -22,7 +21,7 @@ class OpenAITTSVoices:
     NOVA = "nova"
     SHIMMER = "shimmer"
 
-@require_POST
+
 @csrf_exempt
 def generate_audio(request, message_id):
     try:
@@ -36,7 +35,7 @@ def generate_audio(request, message_id):
         # Check if the audio file already exists
         speech_file_path = f"speech_{message_id}.mp3"
         if default_storage.exists(speech_file_path):
-            return JsonResponse({'audio_url': default_storage.url(speech_file_path)})
+            return JsonResponse({"audio_url": default_storage.url(speech_file_path)})
 
         # Call OpenAI text-to-speech API
         openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -47,14 +46,18 @@ def generate_audio(request, message_id):
         )
 
         # Save the audio to a temporary file
-        speech_file_path = default_storage.save(speech_file_path, ContentFile(response.content))
+        speech_file_path = default_storage.save(
+            speech_file_path, ContentFile(response.content)
+        )
 
-        return JsonResponse({'audio_url': default_storage.url(speech_file_path)})
+        return JsonResponse({"audio_url": default_storage.url(speech_file_path)})
 
     except TranscriptMessage.DoesNotExist:
-        return JsonResponse({'error': 'Message not found'}, status=404)
+        return JsonResponse({"error": "Message not found"}, status=404)
     except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
+        print(e)
+        return JsonResponse({"error": str(e)}, status=500)
+
 
 def map_lesson_voice_to_llm_voice(lesson_voice):
     if lesson_voice == "male":
