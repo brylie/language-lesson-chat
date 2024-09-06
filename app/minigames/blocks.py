882e5c146bff0b3e0a2c7d6a12daf6486f7f0f0b
Brylie Christopher Oxley <brylie@protonmail.com>
2024-09-06 15:59:44 +0000

    Add iframe minigame

--- app/minigames/blocks.py
+++ app/minigames/blocks.py
@@ -19,3 +19,32 @@ class StepOrderGameBlock(blocks.StructBlock):
         template = 'minigames/blocks/step_order_game.html'
         icon = 'order'
         label = 'Step Order Game'
+
+
+class QueryParamBlock(blocks.StructBlock):
+    key = blocks.CharBlock(required=True, help_text="Query parameter key")
+    value = blocks.CharBlock(required=True, help_text="Query parameter value")
+
+
+class IframeBlock(blocks.StructBlock):
+    url = blocks.URLBlock(required=True, help_text="URL for the iframe src")
+    query_params = blocks.ListBlock(QueryParamBlock(
+        required=False), help_text="Optional query parameters")
+
+    class Meta:
+        template = 'minigames/blocks/iframe_block.html'
+        icon = 'link'
+        label = 'Iframe'
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        url = value['url']
+        query_params = value['query_params']
+
+        if query_params:
+            query_string = '&'.join(
+                [f"{param['key']}={param['value']}" for param in query_params])
+            url = f"{url}?{query_string}"
+
+        context['iframe_url'] = url
+        return context
